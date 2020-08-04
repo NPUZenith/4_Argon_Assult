7c75fd5a3078012a61fe388eae054d05df0db33b
Zenith <58347499+NPUZenith@users.noreply.github.com>
2020-08-04 16:07:09 -0400

    9_AA_CU2 - Made the music keep playing between scenes

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -8,6 +8,7 @@ public class MusicPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
         Invoke("LoadFirstScene", 2f);
     }
 
