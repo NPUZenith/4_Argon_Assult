c0f35cacb7ead6cdb4287934ca966a97e7950433
Zenith <58347499+NPUZenith@users.noreply.github.com>
2020-08-05 16:45:11 -0400

    25_AA_CU2 - Tidied up the project. Added a seperate script for game loading and removed that action from the music player.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
@@ -9,17 +8,5 @@ public class MusicPlayer : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(gameObject);
-        Invoke("LoadFirstScene", 2f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
