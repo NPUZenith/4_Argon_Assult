ddf5ecd2f106931492f0935a8890ea55198da759
Zenith <58347499+NPUZenith@users.noreply.github.com>
2020-08-05 11:04:27 -0400

    16_AA_CU2 - Added Movement on the y-axis.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,8 +6,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	[Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
-	[Tooltip("In m")][SerializeField] float xRange = 5f;
+	[Tooltip("In ms^-1")][SerializeField] float speed = 20f;
+	[Tooltip("In m")][SerializeField] float xRange = 4f;
+	[Tooltip("In m")][SerializeField] float yRange = 4f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +18,15 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		float xOffset = xThrow * speed * Time.deltaTime;
 		float rawXPos = transform.localPosition.x + xOffset;
 		float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+		
+		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		float yOffset = yThrow * speed * Time.deltaTime;
+		float rawYPos = transform.localPosition.y + yOffset;
+		float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-		transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+		transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
 	}
 }
