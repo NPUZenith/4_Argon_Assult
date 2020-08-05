8ef967726ccd932f49a86d55eb644a2c701c81ea
Zenith <58347499+NPUZenith@users.noreply.github.com>
2020-08-05 10:43:52 -0400

    15_AA_CU2 - Moved the player ship around the x-axis and used Mathf.Clamp() to restrict the movement.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
 	[Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+	[Tooltip("In m")][SerializeField] float xRange = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		float rawXPos = transform.localPosition.x + xOffset;
+		float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+		transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
 	}
 }
