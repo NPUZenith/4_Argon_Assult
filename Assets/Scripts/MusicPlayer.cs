d157fcda0b5843ab233498456f1d8d3ae61ace54
Zenith <58347499+NPUZenith@users.noreply.github.com>
2020-08-05 18:35:08 -0400

    29_AA_CU2 - Made a music player in the main game. Also made it so that it would be destroyed if the music player from the Splash Screen came into the game scene.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -7,6 +7,14 @@ public class MusicPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        int numMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayer > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
