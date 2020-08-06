1068f1ca8145f0514b17dfb89ecaecec2b75feee
NPUZenith <maanav.vish.rao@gmail.com>
2020-08-06 12:16:38 -0700

    40_AA_CU2 - Added a health system for the enemy so that they can take more than one hit of damage.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,9 @@ public class Enemy : MonoBehaviour {
 
 	[SerializeField] GameObject deathFX;
 	[SerializeField] Transform parent;
+	
 	[SerializeField] int scorePerHit = 100;
+	[SerializeField] int hits = 5;
 
 	ScoreBoard scoreBoard;
 
@@ -21,10 +23,25 @@ public class Enemy : MonoBehaviour {
 	}
 
 	void OnParticleCollision(GameObject other)
-	{
+    {
+        ProcessHit();
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hits--;
+        // todo consider hit FX
+    }
+
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
-		fx.transform.parent = parent;
-		scoreBoard.ScoreHit(scorePerHit);
-		Destroy(gameObject);
-	}
+        fx.transform.parent = parent;
+        scoreBoard.ScoreHit(scorePerHit);
+        Destroy(gameObject);
+    }
 }
